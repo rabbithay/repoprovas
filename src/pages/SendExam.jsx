674d527b61e93a81a844448adbdbab3322d71344
rabbithay <thay.csb@gmail.com>
2021-12-18 15:50:17 -0300

    feat: dynamically generate subjects and teacher options

--- src/pages/SendExam.jsx
+++ src/pages/SendExam.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Box, Form, Grommet, FormField, TextInput, Select,
+  Box, Form, Grommet, FormField, TextInput, Select, Text, Button,
 } from 'grommet';
 import { theme } from '../styles/theme';
 import { AppBar } from '../components/AppBar';
+import { subjects } from '../styles/draft';
 
 const defaultValue = {
   title: '',
@@ -15,10 +16,21 @@ const defaultValue = {
 };
 export default function SendExam() {
   const [value, setValue] = useState(defaultValue);
+  const [showMessage, setShowMessage] = useState(false);
+  const [teacherOptions, setTeacherOptions] = useState([]);
+
+  useEffect(() => {
+    const theSubject = subjects.find((sub) => sub.title === value.subject);
+    setTeacherOptions(theSubject?.teachers.map((teacher) => teacher.name) || []);
+  }, [value]);
+
+  const subjectOptions = subjects.map((sub) => sub.title);
 
   const categoryOptions = ['P1', 'P2', 'P3', '2ch', 'Outras'];
-  const subjectOptions = ['matematica', 'fisica', 'quimica'];
-  const teacherOptions = ['P1', 'P2', 'P3', '2ch', 'Outras'];
+
+  const message = (!value.title || !value.category || !value.subject || !value.teacher || !value.link)
+    ? 'É necessário preencher todos os campos'
+    : undefined;
 
   return (
     <Grommet theme={theme} full>
@@ -27,12 +39,18 @@ export default function SendExam() {
         <Box width="medium">
           <Form
             value={value}
-            onChange={(nextValue, { touched }) => {
-              console.log('Change', nextValue, touched);
+            onChange={(nextValue) => {
               setValue(nextValue);
+              console.log('Change ', nextValue);
             }}
-            onReset={() => setValue(defaultValue)}
-            onSubmit={(event) => console.log('Submit', event.value, event.touched)}
+            onReset={() => {
+              setValue(defaultValue);
+              setShowMessage(false);
+            }}
+            onSubmit={(event) => {
+              setShowMessage(!!message);
+              console.log('Submit', event.value, event.touched);
+            }}
           >
             <FormField label="Titulo" name="title">
               <TextInput name="title" placeholder="2020.1" />
@@ -49,6 +67,15 @@ export default function SendExam() {
             <FormField label="Link do PDF" name="link">
               <TextInput name="link" />
             </FormField>
+            {showMessage && (
+              <Box pad={{ horizontal: 'small' }}>
+                <Text color="status-error">{message}</Text>
+              </Box>
+            )}
+            <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+              <Button type="reset" label="Reset" />
+              <Button type="submit" label="Enviar" primary />
+            </Box>
           </Form>
         </Box>
       </Box>
