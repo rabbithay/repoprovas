6f697a0af3fce4d9fc1ec2dce066c9c4de682f00
rabbithay <thay.csb@gmail.com>
2021-12-18 13:38:59 -0300

    refactor: delete invalid import

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,13 +3,11 @@ import {
 } from 'react-router-dom';
 import React from 'react';
 import HomePage from './pages/Home';
-import AddExam from './pages/AddExam';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" element={<HomePage />} />
-      <Route exact path="/add-exam" element={<AddExam />} />
     </Switch>
   );
 }
